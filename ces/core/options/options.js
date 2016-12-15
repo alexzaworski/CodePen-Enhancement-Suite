d3daf50af97b783ab4021019133094d33a67786c
Alex Zaworski <alex.zaworski@gmail.com>
2016-12-15 10:39:08 -0800

    Move CodeMirror markup into separate file, clean up options page

--- ces/core/options/options.js
+++ ces/core/options/options.js
@@ -1,3 +1,18 @@
 (function() {
+  "use strict";
+  var appendPreviewHTML = function() {
+    var previewURL = chrome.extension.getURL("core/options/preview.html");
+    var previewEl = document.getElementById("preview");
+    var xhr = new XMLHttpRequest();
+    
+    xhr.onload = function() {
+      previewEl.innerHTML = xhr.responseText;
+    }
+    
+    xhr.open('GET', previewURL);
+    xhr.send();
+  }
+  
+  appendPreviewHTML();
   cmTheme.init(presets);
 })();
