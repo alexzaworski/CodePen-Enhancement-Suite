89fa5a6d9269b4563f8cd745a40ff0211df2718e
Alex Zaworski <alex.zaworski@gmail.com>
2017-03-10 16:04:28 -0800

    Fix response key

--- ces/src/js/utils/storage.js
+++ ces/src/js/utils/storage.js
@@ -7,7 +7,7 @@ class Storage {
     return new Promise((resolve, reject) => {
       this.store.get(key, (response) => {
         if (response.hasOwnProperty(key)) {
-          resolve(response.key);
+          resolve(response[key]);
         } else {
           reject();
         }
