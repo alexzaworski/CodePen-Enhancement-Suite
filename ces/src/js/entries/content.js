c58e417ed52c43c5b769673211b93b49a062bd07
Alex Zaworski <alex.zaworski@gmail.com>
2017-03-14 15:25:53 -0700

    Refactor distraction free mode

--- ces/src/js/entries/content.js
+++ ces/src/js/entries/content.js
@@ -1,10 +1,12 @@
 import '../../css/content.scss';
 import ResizablePreviews from '../modules/resizablePreviews';
 import RecentPensTypeahead from '../modules/recentPensTypeahead';
-import HideProfileCSS from '../modules/HideProfileCSS';
+import HideProfileCSS from '../modules/hideProfileCSS';
+import DistractionFreeMode from '../modules/distractionFreeMode';
 import CES from '../modules/core/CES';
 
 CES.registerModule(ResizablePreviews);
 CES.registerModule(RecentPensTypeahead);
 CES.registerModule(HideProfileCSS);
+CES.registerModule(DistractionFreeMode);
 CES.initModules();
