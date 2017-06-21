094ad72e7221791da364591b84d7227b22b8f953
Alex Zaworski <alex.zaworski@gmail.com>
2017-06-21 16:57:02 -0700

    Use real CodeMirror in theme preview

--- ces/src/js/entries/options.js
+++ ces/src/js/entries/options.js
@@ -1,13 +1,54 @@
-import getPartial from 'js/utils/getPartial';
+import CodeMirror from 'codemirror';
+
+import 'codemirror/addon/fold/foldgutter';
+import 'codemirror/addon/fold/brace-fold';
+
+import 'codemirror/addon/edit/matchbrackets';
+import 'codemirror/addon/edit/matchtags';
+
+import 'codemirror/mode/javascript/javascript';
+import 'codemirror/mode/css/css';
+import 'codemirror/mode/markdown/markdown';
+
 import dom from 'js/utils/dom';
-import themeGUI from 'js/pages/options/themeGUI';
 import presets from 'js/utils/theme_utils/presets';
 import ensureSyncStorage from 'js/utils/ensureSyncStorage';
 import getActiveTheme from 'js/utils/theme_utils/getActiveTheme';
 
-const prepPage = () => {
-  const preview = dom.get('#preview');
-  getPartial('theme-preview').then(r => preview.html(r));
+import themeGUI from 'js/pages/options/themeGUI';
+import { html, css, js } from 'js/pages/options/codemirror-snippets';
+
+const createCM = ({ mode, selector, value }) => {
+  return CodeMirror(dom.get(selector).node, {
+    value,
+    mode,
+    lineNumbers: true,
+    foldGutter: true,
+    matchBrackets: true,
+    matchTags: true,
+    gutters: ['CodeMirror-linenumbers', 'CodeMirror-foldgutter'],
+    lineWrapping: true
+  });
+};
+
+const initCodeMirror = () => {
+  createCM({
+    mode: 'text/x-markdown',
+    selector: '.box-html',
+    value: html
+  });
+
+  createCM({
+    mode: 'text/x-scss',
+    selector: '.box-css',
+    value: css
+  });
+
+  createCM({
+    mode: 'text/javascript',
+    selector: '.box-js',
+    value: js
+  });
 };
 
 const initGUI = () => {
@@ -15,6 +56,6 @@ const initGUI = () => {
 };
 
 ensureSyncStorage().then(() => {
-  prepPage();
+  initCodeMirror();
   initGUI();
 });
